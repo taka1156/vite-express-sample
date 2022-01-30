98166afa7493c899d7d249d29e131dcbf83ae79b
taka <tomo05061@gmail.com>
2022-01-30 16:22:32 +0900

    update: config

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import { router } from '@/router/v1';
+const PORT = import.meta.env.VITE_SERVER_PORT;
+const PROD = import.meta.env.PROD;
 
 const server = () => {
   const app = express();
-  const PORT = import.meta.env.VITE_SERVER_PORT;
-  
+
   app.use('/api/v1/', router);
+  app.get('/', (req, res) => res.redirect(`${req.baseUrl}/api/v1/`));
   app.listen(PORT);
 
   return app;
 };
 
-export const viteNodeApp = server();
+const viteNodeApp = server();
+
+if (PROD) {
+  console.log(`http://localhost:${PORT}`)
+}
+
+export { viteNodeApp };
--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,26 @@ import { VitePluginNode } from 'vite-plugin-node';
 import { resolve } from 'path';
 
 export default defineConfig({
-    root: 'src',
-    server: {
-        port: 3000,
-        open: '/api/v1/'
+  root: 'src',
+  server: {
+    port: 3000,
+    open: '/api/v1/',
+  },
+  build: {
+    ssr: 'index.ts',
+    outDir: '../api',
+    emptyOutDir: true
+  },
+  plugins: [
+    ...VitePluginNode({
+      adapter: 'express',
+      appPath: './src/index.ts',
+      tsCompiler: 'esbuild',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    plugins: [
-        ...VitePluginNode({
-            adapter: 'express',
-            appPath: './src/index.ts',
-            tsCompiler: 'esbuild'
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src')
-        }
-    }
+  },
 });
